dcfe3b6932e5c01d1f9e2ac39c5e89ad7fea44b1
muralikmr <kurubamurali1999@gmail.com>
2025-09-30 17:40:14 +0530

    all updates

---
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react'
+import '../Pages/HomePage.css'
+import axios from 'axios'
+import Header from '../components/Header'
+ 
+const HomePage = () => {
+
+let [products,setproducts]=useState([])
+
+let[cart,setCart]=useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
+    .then(res=>{
+       setproducts(res.data)
+    }) 
+     
+  },[])
+
+  axios.get('http://localhost:3000/api/cart-items')
+  .then(res=>{
+     setCart(res.data)
+  })
+
+
+ 
+  return (
+    <>
+   <title>Ecommerce-project
+   </title>
+     <Header cart={cart}/>
+
+    <div className="home-page">
+      <div className="products-grid">
+       
+       {
+
+        products.map((elem)=>{
+          return(
+            <div key={elem.id}>
+               <div className="product-container">
+          <div className="product-image-container">
+            <img className="product-image"
+              src={elem.image} />
+          </div>
+
+          <div className="product-name limit-text-to-2-lines">
+           {elem.name}
+          </div>
+
+          <div className="product-rating-container">
+            <img className="product-rating-stars"
+              src={`images/ratings/rating-${elem.rating.stars*10}.png`} />
+            <div className="product-rating-count link-primary">
+              {elem.rating.count}
+            </div>
+          </div>
+
+          <div className="product-price">
+            ${(elem.priceCents/100).toFixed(2)}
+          </div>
+
+          <div className="product-quantity-container">
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div className="product-spacer"></div>
+
+          <div className="added-to-cart">
+            <img src="images/icons/checkmark.png" />
+            Added
+          </div>
+
+          <button className="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+            </div>
+          )
+        })
+
+
+
+
+
+
+       }
+
+       
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
